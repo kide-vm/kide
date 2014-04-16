5bd4be313c1d9f1ff3a47219704acdd1b7961bed
Torsten Ruger <torsten@villataika.fi>
2014-04-16 12:02:28 +0300

    lift label out of the code generator

---
+++ lib/asm/arm/generator_label.rb
@@ -0,0 +1,18 @@
+require "asm/label_object"
+
+class Asm::Arm::GeneratorLabel < Asm::LabelObject
+  def initialize(asm)
+    @asm = asm
+  end
+  def set!
+    @asm.add_object self
+  end
+end
+
+class Asm::Arm::GeneratorExternLabel < Asm::LabelObject
+  def initialize(name)
+    @name = name
+    extern!
+  end
+  attr_reader :name
+end
