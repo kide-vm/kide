2dffad61b8cd099c82f8d8cd4ee58829e59c43b4
Torsten Ruger <torsten@villataika.fi>
2014-04-21 00:07:03 +0300

    fix cp paste

--- test/extern_test.rb
+++ test/extern_test.rb
@@ -10,13 +10,10 @@ class TestExtern < MiniTest::Test
 
   def test_generate_small
     @generator.instance_eval {
-      mov r0, 5                #1
-      loop_start = label
-      loop_start.set!
-      subs r0, r0, 1          #2
-      bne loop_start          #3
-    	mov r7, 1               #4
-    	swi 0                   #5  5 instruction x 4 == 20
+      ldr r0, "hello world"   #1
+      bl :printf              #2
+    	mov r7, 1               #3
+    	swi 0                   #4  4 instruction x 4 == 16
     }
 
     writer = Asm::ObjectWriter.new(Elf::Constants::TARGET_ARM)
