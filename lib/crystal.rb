b1203363d432105a59a5118cf04e09ea90dc128e
Torsten Ruger <torsten@villataika.fi>
2014-04-27 15:34:35 +0300

    vendored parslet, deemed stable enough and better without dependency

--- lib/crystal.rb
+++ lib/crystal.rb
@@ -1,5 +1,3 @@
-# parslet is assumed to be checked out at the same level as crystal for now
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', ".." , "parslet",'lib'))
 require 'parslet'
 
 require "asm/program"
