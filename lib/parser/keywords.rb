583e6f0c9f4a3d7fdff6774492485bd0f5c6a086
Torsten Ruger <torsten@villataika.fi>
2014-04-27 21:12:24 +0300

    add keyword and operators as seperate parser modules

---
+++ lib/parser/keywords.rb
@@ -0,0 +1,9 @@
+module Parser
+  module Keywords
+    include Parslet
+    rule(:keyword_if)   { str('if')   >> space? }
+    rule(:keyword_else) { str('else') >> space? }
+    rule(:keyword_def)  { str('def') >> space? }
+  
+  end
+end
