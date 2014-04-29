8151ed6b9d929981f21a4b65b71b3d10a918d294
Torsten Ruger <torsten@villataika.fi>
2014-04-29 16:57:36 +0300

    moe the last (function) tests to new test model

--- test/parser/test_function_definition.rb
+++ test/parser/test_function_definition.rb
@@ -1,37 +1,36 @@
-require_relative 'helper'
+require_relative "helper"
 
-# Please read note in test_parser
-
-class TransformTest <  MiniTest::Test
-
-  def setup
-    @transform = Parser::Transform.new
-  end
-
-  def check
-    is = @transform.apply @input
-    #puts is.transform
-    assert_equal @transform_output , is
-  end
+class TestFunctionDefinition < MiniTest::Test
+  # include the magic (setup and parse -> test method translation), see there
+  include ParserHelper
   
-  def test_function_definition
-    @input = {:function_definition   => {:name => 'foo'},
-                :parmeter_list => {:parmeter => {:name => 'x'}},
+  def test_simplest_function
+    @string_input    = <<HERE
+def foo(x) 
+  5
+end
+HERE
+    @parse_output = {:function_definition   => {:name => 'foo'},
+                :parmeter_list => [{:parmeter => {:name => 'x'}}],
                 :expressions   => [{:integer => '5'}]}
     @transform_output = Parser::FunctionExpression.new('foo', 
                 [Parser::NameExpression.new('x')], 
                 [Parser::IntegerExpression.new(5)])
-    check
   end
 
   def test_function_assignment
-    @input = { :function_definition => { :name => "foo" } , 
-                  :parmeter_list => { :parmeter => { :name => "x" } }, 
+    @string_input    = <<HERE
+def foo(x) 
+ abba = 5 
+end
+HERE
+    @parse_output = { :function_definition => { :name => "foo" } , 
+                  :parmeter_list => [{ :parmeter => { :name => "x" } }], 
                   :expressions => [ { :asignee => { :name => "abba" }, :asigned => { :integer => "5" } } ]
                 }
     @transform_output = Parser::FunctionExpression.new( "foo", [Parser::NameExpression.new("x")],
                            [Parser::AssignmentExpression.new( "abba", Parser::IntegerExpression.new(5) ) ])
-    check
   end
 
+    
 end
