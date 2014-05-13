140d33b167a744c9f6c449cb3007d40533e8a0f7
Torsten Ruger <torsten@villataika.fi>
2014-05-13 10:36:44 +0300

    c fibs (need to loop a lot to use time on it)

---
+++ test/bench/fibo.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int fibo(int n){
+  int result;
+	int a = 0;
+	int b = 1;
+	int i = 1;
+  while( i < n )
+  {
+      result = a + b;
+      a = b;
+      b = result;
+      i++;
+  }
+	return result;
+}
+
+int main(void)
+{
+	int counter = 1000000;
+	int counter2 = counter;
+	int level = 76;
+	int fib ;
+	while(counter--) { 
+		fib = fibo(level);
+	}
+	printf("%i times fib %d \n",counter2 , level);
+		
+}
