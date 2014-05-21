bc278a61ebf2abe41bad694c4450953e65dd9da7
Torsten Ruger <torsten@villataika.fi>
2014-05-21 21:12:46 +0300

    using function args for call

--- lib/vm/call_site.rb
+++ lib/vm/call_site.rb
@@ -14,9 +14,9 @@ module Vm
     def load_args into
       args.each_with_index do |arg , index|
         if arg.is_a?(IntegerConstant) or arg.is_a?(StringConstant)
-          Vm::Integer.new(index).load into , arg
+          function.args[index].load into , arg
         else
-          Vm::Integer.new(index).move( into, arg ) if arg.register != index
+          function.args[index].move( into, arg ) if arg.register != index
         end
       end
     end
