5e8688c4170c54d31b5444e4d0157ad2ed37f4a2
Torsten Ruger <torsten@villataika.fi>
2014-05-28 21:00:52 +0300

    small fixes to get all the tests to pass/run

--- test/fragments/helper.rb
+++ test/fragments/helper.rb
@@ -37,7 +37,7 @@ module Fragments
     writer = Elf::ObjectWriter.new(@program , Elf::Constants::TARGET_ARM)
     assembly = writer.text
     # use this for getting the bytes to compare to :  
-    puts assembly
+    #puts assembly
     writer.save("#{name}.o")
     assembly.text.bytes.each_with_index do |byte , index|
       is = @should[index]
