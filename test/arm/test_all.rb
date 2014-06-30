84d27ce9d98c1f5015946115a01bf41bb129cd24
Torsten Ruger <torsten@villataika.fi>
2014-06-30 15:28:39 +0300

    remove arm as it is in own rep now

--- test/arm/test_all.rb
+++
@@ -1,6 +0,0 @@
-require_relative "test_stack"
-require_relative "test_control"
-require_relative "test_logic"
-require_relative "test_move"
-require_relative "test_memory"
-require_relative "test_compare"
--- test/test_all.rb
+++ test/test_all.rb
@@ -1,3 +1,2 @@
-require_relative "arm/test_all"
 require_relative "fragments/test_all"
 require_relative "test_intel"
