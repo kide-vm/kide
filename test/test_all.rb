0271045ff49fd1f4aeb14def81dc2d8776bebe94
Torsten Ruger <torsten@villataika.fi>
2014-08-30 20:46:38 +0300

    corrected unit name

--- test/test_all.rb
+++ test/test_all.rb
@@ -1,6 +1,6 @@
 # All working tests (ahm), still working on the others, so no use to be constantly reminded
 require_relative "arm/test_all"
-require_relative "unit/hash"
+require_relative "parfait/hash"
 require_relative "sof"
 
 #require_relative "virtual/test_all"
