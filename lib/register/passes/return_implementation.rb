4f7fefb0abb890923f3b358d585f92a2d21e7700
Torsten Ruger <torsten@villataika.fi>
2014-10-04 12:52:28 +0300

    using new register instructions

--- lib/register/passes/return_implementation.rb
+++ lib/register/passes/return_implementation.rb
@@ -4,17 +4,16 @@ module Register
       block.codes.dup.each do |code|
         next unless code.is_a? Virtual::MethodReturn
         new_codes = []
-        machine = RegisterMachine.instance
         slot = Virtual::Slot
         # move the current message to new_message
-        new_codes << machine.mov( slot::MESSAGE_REGISTER , slot::NEW_MESSAGE_REGISTER )
+        new_codes << RegisterTransfer.new( slot::MESSAGE_REGISTER , slot::NEW_MESSAGE_REGISTER )
         # and restore the message from saved value in new_message
-        new_codes << machine.ldr( slot::MESSAGE_REGISTER ,slot::NEW_MESSAGE_REGISTER , slot::MESSAGE_CALLER )
+        new_codes << GetSlot.new( slot::MESSAGE_REGISTER , slot::NEW_MESSAGE_REGISTER , slot::MESSAGE_CALLER )
         # "roll out" self and frame into their registers
-        new_codes << machine.ldr( slot::SELF_REGISTER ,slot::MESSAGE_REGISTER , slot::MESSAGE_SELF )
-        new_codes << machine.ldr( slot::FRAME_REGISTER ,slot::MESSAGE_REGISTER , slot::MESSAGE_FRAME )
+        new_codes << GetSlot.new( slot::SELF_REGISTER ,slot::MESSAGE_REGISTER , slot::MESSAGE_SELF )
+        new_codes << GetSlot.new( slot::FRAME_REGISTER ,slot::MESSAGE_REGISTER , slot::MESSAGE_FRAME )
         #load the return address into pc, affecting return. (other cpus have commands for this, but not arm)
-        new_codes << machine.ldr( :pc ,slot::MESSAGE_REGISTER , slot::MESSAGE_RETURN_ADDRESS )
+        new_codes << FunctionReturn.new( slot::MESSAGE_REGISTER , slot::MESSAGE_RETURN_ADDRESS )
         block.replace(code , new_codes )
       end
     end
--- lib/virtual/passes/enter_implementation.rb
+++ lib/virtual/passes/enter_implementation.rb
@@ -1,4 +1,4 @@
-module Register
+module Virtual
 
   class EnterImplementation
     def run block
@@ -7,11 +7,11 @@ module Register
         new_codes = []
         # save return register and create a new frame
         # lr is link register, ie where arm stores the return address when call is issued
-        new_codes << RegisterMachine.instance.str( :lr , Virtual::Slot::MESSAGE_REGISTER , Virtual::Slot::MESSAGE_RETURN_ADDRESS )
+        new_codes << Register::SaveReturn.new( Virtual::Slot::MESSAGE_REGISTER , Virtual::Slot::MESSAGE_RETURN_ADDRESS )
         new_codes << Virtual::NewFrame.new
         block.replace(code , new_codes )
       end
     end
   end
-  Virtual::BootSpace.space.add_pass_after "Register::EnterImplementation" , "Virtual::GetImplementation"
+  Virtual::BootSpace.space.add_pass_after "Virtual::EnterImplementation" , "Virtual::GetImplementation"
 end
