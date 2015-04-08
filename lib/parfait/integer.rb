81e3a98af8ecc897e373b88af0f359eeb01e1cac
Torsten Ruger <torsten@villataika.fi>
2015-04-08 20:32:56 +0300

    add integer to parfait

---
+++ lib/parfait/integer.rb
@@ -0,0 +1,12 @@
+
+# Integer class for representing mathods on Integers
+# Integers are Values (not Objects), 
+# - they have fixed value
+# - they are immutable
+# you can *not* assign instance variables or methods
+
+# TODO how this idea works with Numeric ?
+
+class Integer < Value
+
+end
