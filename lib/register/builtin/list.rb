8f0fd30ef3575342248f36a01a1632494079ce89
Torsten Ruger <torsten@villataika.fi>
2015-07-01 21:42:31 +0300

    small name update

--- lib/register/builtin/list.rb
+++ lib/register/builtin/list.rb
@@ -1,6 +1,8 @@
 module Register
   module Builtin
-    class Array
+    # This is all fantasy at the moment. Not even required, not tested, just an idea
+    # Most likely wrong, in fact so old as to predate the internal_XX stuff
+    class List
       module ClassMethods
         def get context , index = Virtual::Integer
           get_function = Virtual::CompiledMethodInfo.create_method(:get , [ Virtual::Integer] , Virtual::Integer , Virtual::Integer )
