ac9d5eaf5a18f08c626ad0cda2ef71ce3fe3c6a7
Torsten Ruger <torsten@villataika.fi>
2015-07-19 10:45:12 +0300

    fix tests by reordering (hmm)

--- test/parfait/test_all.rb
+++ test/parfait/test_all.rb
@@ -1,5 +1,5 @@
+require_relative "test_space"
 require_relative "test_object"
 require_relative "test_list"
 require_relative "test_word"
 require_relative "test_dictionary"
-require_relative "test_space"
