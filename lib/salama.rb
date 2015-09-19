9bacb07f1cdd8213fda71a4b67b87628231a057d
Torsten Ruger <torsten@villataika.fi>
2015-09-19 17:57:31 +0300

    add some ast sugar

--- lib/salama.rb
+++ lib/salama.rb
@@ -3,6 +3,17 @@ require 'parslet'
 require "stream_reader"
 require "elf/object_writer"
 require 'salama-reader'
+AST::Node.class_eval do
+  def each
+    children.each do |child|
+      yield child
+    end
+  end
+  def first
+    children.first
+  end
+end
+
 require 'parser/transform'
 require "salama-object-file"
 require "virtual"
