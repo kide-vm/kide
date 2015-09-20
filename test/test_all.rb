4b613fb6323ef857d0e5cedee13096776a7f52cf
Torsten Ruger <torsten@villataika.fi>
2015-09-21 00:14:47 +0300

    virtual tests back

--- test/test_all.rb
+++ test/test_all.rb
@@ -4,6 +4,6 @@ require_relative "parfait/test_all"
 
 require_relative "fragments/test_all"
 
-#require_relative "virtual/test_all"
+require_relative "virtual/test_all"
 
 require_relative "interpreter/test_all"
