c1d31a541da45df289cd3dd6038db587371894dc
Torsten Ruger <torsten@villataika.fi>
2015-10-07 10:43:08 +0300

    improve method test
    mostly by actually testing main, which is what is checked

--- test/compiler/test_methods.rb
+++ test/compiler/test_methods.rb
@@ -5,44 +5,34 @@ module Virtual
   class TestMethods < MiniTest::Test
     include CodeChecker
 
-    def test_module
+    def test_simplest_function
       @string_input    = <<HERE
-class Some
-  int foo()
+class Object
+  int main()
     return 5
   end
 end
 HERE
-      @output = [[MethodEnter] ,[MethodReturn]]
+      @output = [[MethodEnter,Set] ,[MethodReturn]]
       check
     end
 
-    def test_simplest_function
+
+    def test_second_simplest_function
       @string_input    = <<HERE
 class Object
-  int foo(int x)
+  int main()
+    int x = 5
     return x
   end
 end
 HERE
-      @output = [[MethodEnter] ,[MethodReturn]]
+      @output = [[Virtual::MethodEnter,Set],[Virtual::MethodReturn]]
       check
     end
 
-  def test_second_simplest_function
-    @string_input    = <<HERE
-class Object
-  ref foo(ref x)
-    return x
-  end
-end
-HERE
-    @output = [[Virtual::MethodEnter],[Virtual::MethodReturn]]
-    check
-  end
-
-  def test_puts_string
-    @string_input    = <<HERE
+    def test_puts_string
+      @string_input    = <<HERE
 class Object
   int puts(ref str)
     return str
@@ -52,26 +42,26 @@ class Object
   end
 end
 HERE
-    @output = [[MethodEnter ,  NewMessage, Set, Set , Set, Set, MethodCall],[MethodReturn]]
-    check
-  end
+      @output = [[MethodEnter ,  NewMessage, Set, Set , Set, Set, MethodCall],[MethodReturn]]
+      check
+    end
 
-  def test_int_function
-    @string_input    = <<HERE
+    def test_int_function
+      @string_input    = <<HERE
 class Integer < Object
 int times(int x)
   self * x
 end
 end
 HERE
-    @output = [[Virtual::MethodEnter] , [Virtual::MethodReturn]]
-    check
-    cla = Virtual.machine.space.get_class_by_name :Integer
-    assert cla.get_instance_method( :times )
-  end
+      @output = [[Virtual::MethodEnter] , [Virtual::MethodReturn]]
+      check
+      cla = Virtual.machine.space.get_class_by_name :Integer
+      assert cla.get_instance_method( :times )
+    end
 
-  def test_function_ops
-    @string_input    = <<HERE
+    def test_function_ops
+      @string_input    = <<HERE
 class Object
   int foo(int abba)
    abba = 5
@@ -79,28 +69,29 @@ class Object
   end
 end
 HERE
-    @output = [[Virtual::MethodEnter] , [Virtual::MethodReturn]]
-    check
-  end
+      @output = [[Virtual::MethodEnter] , [Virtual::MethodReturn]]
+      check
+    end
 
-  def test_function_if
-    @string_input    = <<HERE
+    def test_function_if
+      @string_input    = <<HERE
 class Object
-  int ofthen(int n)
+  int main()
     if(0)
-      int isit = 42
+      return 42
     else
-      int maybenot = 667
+      return 667
     end
   end
 end
 HERE
-    @output = [[Virtual::MethodEnter] , [Virtual::MethodReturn]]
-    check
-  end
+      @output = [[MethodEnter,Set,Register::IsZeroBranch] , [Set,Register::AlwaysBranch],
+                  [Set],[],[MethodReturn]]
+      check
+    end
 
-  def test_while
-    @string_input    = <<HERE
+    def test_while
+      @string_input    = <<HERE
 class Object
   int foo()
     while(1)
@@ -109,12 +100,12 @@ class Object
   end
 end
 HERE
-    @output = [[Virtual::MethodEnter],[Virtual::MethodReturn]]
-    check
-  end
+      @output = [[Virtual::MethodEnter],[Virtual::MethodReturn]]
+      check
+    end
 
-  def test_function_while
-    @string_input    = <<HERE
+    def test_function_while
+      @string_input    = <<HERE
 class Object
   int fibonaccit(int n)
     int a = 0
@@ -125,12 +116,12 @@ class Object
   end
 end
 HERE
-    @output = [[Virtual::MethodEnter],[Virtual::MethodReturn]]
-    check
-  end
+      @output = [[Virtual::MethodEnter],[Virtual::MethodReturn]]
+      check
+    end
 
-  def test_function_return
-    @string_input    = <<HERE
+    def test_function_return
+      @string_input    = <<HERE
 class Object
   int retvar(int n)
     int i = 5
@@ -138,14 +129,15 @@ class Object
   end
 end
 HERE
-    @output = [[Virtual::MethodEnter],[Virtual::MethodReturn]]
-    check
-  end
+      @output = [[Virtual::MethodEnter],[Virtual::MethodReturn]]
+      check
+    end
 
-  def test_function_return_if
-    @string_input    = <<HERE
+    def test_function_return_if
+      @string_input    = <<HERE
 class Object
-  int retvar(int n)
+  int main()
+    int n = 10
     if( n > 5)
       return 10
     else
@@ -154,46 +146,30 @@ class Object
   end
 end
 HERE
-    @output = [[Virtual::MethodEnter],[Virtual::MethodReturn]]
-    check
-  end
+      @output = [[MethodEnter,Set,Set,Register::GetSlot,Register::GetSlot,
+                  Register::OperatorInstruction,Register::IsZeroBranch],
+                  [Set,Register::AlwaysBranch],[Set],[],[MethodReturn]]
+      check
+    end
 
-  def test_function_return_while
-    @string_input    = <<HERE
+    def test_function_return_while
+      @string_input    = <<HERE
 class Object
-  int retvar(int n)
-    while( n > 5) do
+  int main()
+    int n = 10
+    while( n > 5)
       n = n + 1
       return n
     end
   end
 end
 HERE
-    @output = [[Virtual::MethodEnter],[Virtual::MethodReturn]]
-    check
-  end
-
-  def test_function_big_while
-    @string_input    = <<HERE
-class Object
-  int puts(int i)
-    return 0
-  end
-  int fibonaccit(int n)
-    int a = 0
-    int b = 1
-    while( n > 1 )
-      int tmp = a
-      a = b
-      b = tmp + b
-      puts(b)
-      n = n - 1
+      @output = [[MethodEnter,Set],
+                 [Set,Register::GetSlot,Register::GetSlot,Register::OperatorInstruction,
+                   Register::IsZeroBranch,Set,Register::GetSlot,Register::GetSlot,
+                   Register::OperatorInstruction,Set,Register::AlwaysBranch] ,
+                   [],[MethodReturn]]
+      check
     end
   end
-end
-HERE
-    @output = [[Virtual::MethodEnter],[Virtual::MethodReturn]]
-    check
-  end
-end
 end
