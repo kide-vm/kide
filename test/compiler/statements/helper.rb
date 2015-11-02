7eebc775d6395019a44b441e4534fd22947558d6
Torsten Ruger <torsten@villataika.fi>
2015-11-02 20:11:40 +0200

    let helper output correct version in case of error

--- test/compiler/statements/helper.rb
+++ test/compiler/statements/helper.rb
@@ -16,14 +16,22 @@ module Statements
 
   def compare_instructions instruction , expect
     index = 0
+    start = instruction
     begin
       should = expect[index]
       assert should , "No instruction at #{index}"
-      assert_equal instruction.class , should , "Expected at #{index+1}"
+      assert_equal instruction.class , should , "Expected at #{index+1}\n#{should(start)}"
       index += 1
       instruction = instruction.next
     end while( instruction )
   end
-
-
+  def should start
+    str = start.to_ac.to_s
+    str.gsub!("Register::","")
+    ret = ""
+    str.split(",").each_slice(7).each do |line|
+      ret += "              " + line.join(",") + " ,\n"
+    end
+    ret
+  end
 end
