cffbc918210d71fc75f7f797ba21494534b3f2ba
Torsten Ruger <torsten@villataika.fi>
2015-11-02 21:27:37 +0200

    fix field_def to new syntax

--- lib/soml/compiler/field_def.rb
+++ lib/soml/compiler/field_def.rb
@@ -6,9 +6,9 @@ module Soml
       reset_regs # field_def is a statement, no return and all regs
       #puts statement.inspect
       type , name , value = *statement
-      @method.ensure_local( name, type ) unless( @method.has_arg(name))
+      @method.ensure_local( name.first, type ) unless( @method.has_arg(name.first))
       # if there is a value assigned, process it as am assignemnt statement (kind of call on_assign)
-      process( s(:assignment , s(:name , name) , value )  ) if value
+      process( s(:assignment , name , value )  ) if value
       return nil
     end
   end
