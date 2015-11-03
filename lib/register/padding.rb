0f8f0a681cd443d9bf6b9ae6849b472e47341b44
Torsten Ruger <torsten@villataika.fi>
2015-11-03 16:22:50 +0200

    fix padding to only consider layout (1 word)

--- lib/register/padding.rb
+++ lib/register/padding.rb
@@ -2,10 +2,10 @@
 module Padding
 
   # objects only come in lengths of multiple of 8 words
-  # but there is a constant overhead of 2 words, one for type, one for layout
+  # but there is a constant overhead of 1 words for layout
   # and as we would have to subtract 1 to make it work without overhead, we now have to add 7
   def padded len
-    a = 32 * (1 + (len + 7)/32 )
+    a = 32 * (1 + (len + 3)/32 )
     #puts "#{a} for #{len}"
     a
   end
@@ -15,7 +15,7 @@ module Padding
   end
 
   def padding_for length
-    pad = padded(length) - length - 8 # for header, type and layout
+    pad = padded(length) - length - 4 # for header, layout
     pad
   end
 end
