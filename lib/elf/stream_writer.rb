661911de0aad1b240e87c02b9ee0c19a9afd79b4
Torsten Ruger <torsten@villataika.fi>
2016-12-10 23:45:24 +0200

    comment unused fluff

--- lib/elf/stream_writer.rb
+++ lib/elf/stream_writer.rb
@@ -6,46 +6,46 @@ module StreamWriter
   def write_uint32(*args)
     return write_binary(args,'L')
   end
-  def write_uint16(*args)
-    return write_binary(args,'S')
-  end
   def write_uint8(*args)
-    return write_binary(args,'C')
+   return write_binary(args,'C')
   end
-  def write_uint64(*args)
-    return write_binary(args,'Q')
-  end
-  def write_sint64(*args)
-    return write_binary(args,'q')
-  end
-  def write_cstr_fixed(str, len)
-    return __sr_write(str.ljust(len, 0.chr))
-  end
-  def write_cstr_terminated(str)
-    return __sr_write(str + 0.chr)
-  end
-  def write_cstr_prefixed(str)
-    write_uint8(str.word_length)
-    return __sr_write(str)
-  end
-  def write_str(str)
-    return __sr_write(str)
-  end
-  def write_float(*args)
-    return write_binary(args,'F')
-  end
-  def write_double(*args)
-    return write_binary(args,'D')
-  end
-  def write_sint16(*args)
-    return write_binary(args,'s')
+  def write_uint16(*args)
+   return write_binary(args,'S')
   end
   def write_sint32(*args)
     return write_binary(args,'l')
   end
-  def write_data(str)
-    return __sr_write(str)
-  end
+  # def write_uint64(*args)
+  #   return write_binary(args,'Q')
+  # end
+  # def write_sint64(*args)
+  #   return write_binary(args,'q')
+  # end
+  # def write_cstr_fixed(str, len)
+  #   return __sr_write(str.ljust(len, 0.chr))
+  # end
+  # def write_cstr_terminated(str)
+  #   return __sr_write(str + 0.chr)
+  # end
+  # def write_cstr_prefixed(str)
+  #   write_uint8(str.word_length)
+  #   return __sr_write(str)
+  # end
+  # def write_str(str)
+  #   return __sr_write(str)
+  # end
+  # def write_float(*args)
+  #   return write_binary(args,'F')
+  # end
+  # def write_double(*args)
+  #   return write_binary(args,'D')
+  # end
+  # def write_sint16(*args)
+  #   return write_binary(args,'s')
+  # end
+  # def write_data(str)
+  #   return __sr_write(str)
+  # end
 end
 
 class IO
