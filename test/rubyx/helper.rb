d2d845266e59e93b6945275fca566ff63e5355f2
Torsten Ruger <torsten@villataika.fi>
2017-04-12 20:58:40 +0300

    remove code that was moved to support helper

--- test/rubyx/helper.rb
+++ test/rubyx/helper.rb
@@ -1,18 +1,1 @@
 require_relative '../helper'
-
-module Rubyx
-  module CompilerHelper
-
-    def in_Test(statements)
-      "class Test ; #{statements} ; end"
-    end
-    
-    def in_Space(statements)
-      "class Space ; #{statements} ; end"
-    end
-
-    def as_main(statements)
-      in_Space("def main ; #{statements}; end")
-    end
-  end
-end
