0e98179888946e6df2002f94e29e65e80733f306
Torsten Ruger <torsten@villataika.fi>
2017-09-06 12:08:44 +0300

    fold the mini check file

--- lib/mom/check.rb
+++
@@ -1,9 +0,0 @@
-module Mom
-
-  # A base class for conditions in MOM
-  # Just a marker, no real functionality for now
-
-  class Check < Instruction
-
-  end
-end
--- lib/mom/truth_check.rb
+++ lib/mom/truth_check.rb
@@ -1,6 +1,11 @@
-require_relative "check"
-
 module Mom
+
+  # A base class for conditions in MOM
+  # Just a marker, no real functionality for now
+
+  class Check < Instruction
+
+  end
 
   # The funny thing about the ruby truth is that is is anything but false or nil
   #
@@ -11,7 +16,7 @@ module Mom
     attr_reader :condition
 
     def initialize(condition)
-      @condition  = condition 
+      @condition  = condition
     end
   end
 end
