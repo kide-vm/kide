a523af14f1e77a1ba779ac85835077f9ceb89deb
Torsten Ruger <torsten@villataika.fi>
2017-09-10 12:36:16 +0300

    fix mom assignment test

--- test/mom/test_assignment.rb
+++ test/mom/test_assignment.rb
@@ -2,15 +2,24 @@ require_relative "helper"
 
 module Mom
   class TestAssignemnt < MiniTest::Test
-    include CompilerHelper
+    include MomCompile
 
     def setup
       Risc.machine.boot
+      @stats = compile_first_method_flat( "@a = 5")
     end
 
-    def compile_first input
-      lst = Vool::VoolCompiler.compile in_Space( input )
-      lst.to_mom( nil )
+    def test_if_compiles
+      assert_equal SlotConstant , @stats.class , @stats
+    end
+    def test_length
+      assert_equal 1 , @stats.length
+    end
+    def test_assigns_class
+      assert_equal Vool::IntegerStatement , @stats.right.class , @stats.inspect
+    end
+    def test_assigns_value
+      assert_equal 5 , @stats.right.value , @stats.inspect
     end
 
   end
