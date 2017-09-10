94ca8da35448c2eacfc594f7e5ead188999e71db
Torsten Ruger <torsten@villataika.fi>
2017-09-10 19:48:46 +0300

    improve mom assignment tests

--- test/mom/test_assignment.rb
+++ test/mom/test_assignment.rb
@@ -1,7 +1,7 @@
 require_relative "helper"
 
 module Mom
-  class TestAssignemnt < MiniTest::Test
+  class TestConstToIvarAssignemnt < MiniTest::Test
     include MomCompile
 
     def setup
@@ -22,5 +22,34 @@ module Mom
       assert_equal 5 , @stats.right.value , @stats.inspect
     end
 
+  end
+  class TestConstToLocalAssignemnt < MiniTest::Test
+    include MomCompile
+    def setup
+      Risc.machine.boot
+      @stats = compile_first_method_flat( "a = 5")
+    end
+    def test_if_compiles
+      assert_equal SlotConstant , @stats.class , @stats
+    end
+    def test_length
+      assert_equal 1 , @stats.length
+    end
+  end
+  class TestIvarToLocalAssignemnt < MiniTest::Test
+    include MomCompile
+    def setup
+      Risc.machine.boot
+      @stats = compile_first_method_flat( "a = @a")
+    end
+    def test_if_compiles
+      assert_equal SlotMove , @stats.class , @stats
+    end
+    def test_length
+      assert_equal 1 , @stats.length
+    end
+    def test_assigns_class
+      assert_equal Vool::InstanceVariable , @stats.right.class , @stats.inspect
+    end
   end
 end
