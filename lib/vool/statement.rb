2779045caa304937af49b2f42a70b7846f5aca2c
Torsten Ruger <torsten@villataika.fi>
2018-03-13 16:44:39 +0530

    small rename

--- lib/vool/statement.rb
+++ lib/vool/statement.rb
@@ -54,12 +54,12 @@ module Vool
 end
 
 
+require_relative "statements/assign_statement"
 require_relative "statements/array_statement"
 require_relative "statements/basic_values"
 require_relative "statements/class_statement"
 require_relative "statements/hash_statement"
 require_relative "statements/if_statement"
-require_relative "statements/ivar_statement"
 require_relative "statements/logical_statement"
 require_relative "statements/local_statement"
 require_relative "statements/method_statement"
