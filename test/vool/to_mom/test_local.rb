9b4fcf0e0f796dd869dd1a96984c0baddd8ead15
Torsten Ruger <torsten@villataika.fi>
2018-03-15 20:56:27 +0530

    locals work

--- test/vool/to_mom/test_local.rb
+++ test/vool/to_mom/test_local.rb
@@ -6,33 +6,32 @@ module Vool
 
     def setup
       Risc.machine.boot
-      @stats = compile_first_method( "a = 5")
-      @first = @stats.first
+      @ins = compile_first_method( "a = 5")
     end
 
     def test_compiles_not_array
-      assert Array != @stats.class , @stats
+      assert Array != @ins.class , @ins
     end
     def test_class_compiles
-      assert_equal Mom::SlotLoad , @first.class , @stats
+      assert_equal Mom::SlotLoad , @ins.class , @ins
     end
     def test_slot_is_set
-      assert @first.left
+      assert @ins.left
     end
     def test_slot_starts_at_message
-      assert_equal :message , @first.left.known_object
+      assert_equal :message , @ins.left.known_object
     end
     def test_slot_gets_frame
-      assert_equal :frame , @first.left.slots[0]
+      assert_equal :frame , @ins.left.slots[0]
     end
     def test_slot_assigns_to_local
-      assert_equal :a , @first.left.slots[-1]
+      assert_equal :a , @ins.left.slots[-1]
     end
     def test_slot_assigns_something
-      assert @first.right
+      assert @ins.right
     end
     def test_slot_assigns_int
-      assert_equal Mom::IntegerConstant ,  @first.right.class
+      assert_equal Mom::IntegerConstant ,  @ins.right.class
     end
   end
 end
