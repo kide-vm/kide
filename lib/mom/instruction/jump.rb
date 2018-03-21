b99fdc3425d4e9e43c5e2de717f45cfd5a0cea83
Torsten Ruger <torsten@villataika.fi>
2018-03-21 11:52:53 +0530

    rename jump label

--- lib/mom/instruction/jump.rb
+++ lib/mom/instruction/jump.rb
@@ -1,18 +1,18 @@
 module Mom
 
-  # Unconditional jump to the Label given as target
+  # Unconditional jump to the Label given
   # Eg used at the end of while or end of if_true branch
   #
   # Risc equivalent is the same really, called Unconditional there.
   #
   class Jump < Instruction
-    attr_reader :target
+    attr_reader :label
 
-    def initialize(target)
-      @target = target
+    def initialize(label)
+      @label = label
     end
     def to_risc(compiler)
-      Risc::Unconditional.new(self , @target.to_risc(compiler))
+      Risc::Unconditional.new(self , @label.to_risc(compiler))
     end
   end
 
