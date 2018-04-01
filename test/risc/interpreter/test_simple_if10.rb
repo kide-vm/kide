599679d6e83ce52cf954518596c8cbe374c9bc3f
Torsten Ruger <torsten@villataika.fi>
2018-04-01 22:42:27 +0300

    simplest if
    without true/false or logic

---
+++ test/risc/interpreter/test_simple_if10.rb
@@ -0,0 +1,31 @@
+require_relative "helper"
+
+module Risc
+  class InterreterSimpleIf10 < MiniTest::Test
+    include Ticker
+
+    def setup
+      @string_input = as_main 'if( 10 ); return "then";else;return "else";end'
+      super
+    end
+
+    def test_if
+        #show_ticks # get output of what is
+        check_chain [Branch, Label, LoadConstant, SlotToReg, LoadConstant,
+             SlotToReg, SlotToReg, RegToSlot, LoadConstant, SlotToReg,
+             SlotToReg, SlotToReg, SlotToReg, RegToSlot, LoadConstant,
+             SlotToReg, SlotToReg, SlotToReg, SlotToReg, RegToSlot,
+             SlotToReg, RegToSlot, LoadConstant, RegToSlot, FunctionCall,
+             Label, LoadConstant, LoadConstant, OperatorInstruction, IsNotZero,
+             Label, LoadConstant, RegToSlot, SlotToReg, SlotToReg,
+             RegToSlot, SlotToReg, SlotToReg, FunctionReturn, Transfer,
+             Syscall, NilClass]
+      assert_equal Parfait::Word , get_return.class
+      assert_equal "else" , get_return.to_string
+    end
+    def test_exit
+      done = ticks(41)
+      assert_equal Syscall ,  done.class
+    end
+  end
+end
