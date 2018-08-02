4d4b691a4b2bf8b3f1f60c811ec60b8e78e058a8
Torsten Ruger <torsten@villataika.fi>
2018-08-02 17:34:44 +0300

    adds a return jump
    that will be used to jump to the return sequence

---
+++ lib/mom/instruction/return_jump.rb
@@ -0,0 +1,16 @@
+module Mom
+
+  # the return jump jumps to the return label
+  # the method setup is such that there is exactly one return_label in a method
+  # This is so the actual code that executes the return can be quite complicated
+  # and big, and won't be repeated
+  #
+  class ReturnJump < Instruction
+
+    def to_risc(compiler)
+      Risc::Branch.new(self , compiler.return_label)
+    end
+  end
+
+
+end
